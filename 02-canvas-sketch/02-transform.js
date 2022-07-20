1d739ddc0b77efaef9ec33434fc417185382d7f0
Shashank Sharma <shashank.ns67@gmail.com>
2022-07-20 16:14:47 +0530

    rotating

--- 02-canvas-sketch/02-transform.js
+++ 02-canvas-sketch/02-transform.js
@@ -12,23 +12,36 @@ const sketch = () => {
 
     context.fillStyle = 'black';
 
-    const x = width * 0.5;
-    const y = height * 0.5;
-    const w = width * 0.3;
-    const h = height * 0.3;
+    const cx = width * 0.5;
+    const cy = height * 0.5;
+    const w = width * 0.01;
+    const h = height * 0.1;
+    let x, y;
 
-    // to save the current context state
-    context.save();
-    
-    context.translate(x, y);
-    context.rotate(toRad(45));
+    const num = 12;
+    const radius = width * 0.3;
 
-    context.beginPath();
-    context.fillRect(-w * 0.5, -h * 0.5, w, h);
-    context.fill();
-    
-    // to restore the saved context state
-    context.restore();
+    for (let i = 0; i < num; i++) {
+      const slice = toRad(360 / num);
+      const angle = slice * i;
+      
+      x = cx + radius * Math.sin(angle);
+      y = cy + radius * Math.cos(angle);
+
+      // to save the current context state
+      context.save();
+      
+      context.translate(x, y);
+      context.rotate(-angle);
+  
+      context.beginPath();
+      context.fillRect(-w * 0.5, -h * 0.5, w, h);
+      context.fill();
+      
+      // to restore the saved context state
+      context.restore();
+    }
+
   };
 };
 
