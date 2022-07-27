bbe1f6e82d7868a4f15b5d82783d2fdf333340e7
Shashank Sharma <shashank.ns67@gmail.com>
2022-07-27 16:46:29 +0530

    animation

--- 02-canvas-sketch/03-objects.js
+++ 02-canvas-sketch/03-objects.js
@@ -2,10 +2,12 @@ const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random');
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [ 1080, 1080 ],
+  animate: true,
+  fps: 60
 };
 
-class Point {
+class Vector {
   constructor(x, y) {
     this.x = x;
     this.y = y;
@@ -14,16 +16,37 @@ class Point {
 
 class Agent {
   constructor(x, y) {
-    this.pos = new Point(x, y);
-    this.radius = 10;
+    this.pos = new Vector(x, y);
+    this.radius = random.range(4, 12);
+    this.vel = new Vector(random.range(-1, 1), random.range(-1, 1));
+  }
+
+  bounce(width, height) {
+    if (this.pos.x <= 0 || this.pos.x >= width) {
+      this.vel.x *= -1;
+    }
+    if (this.pos.y <= 0 || this.pos.y >= width) {
+      this.vel.y *= -1;
+    }
+  }
+
+  update() {
+    this.pos.x += this.vel.x;
+    this.pos.y += this.vel.y;
   }
 
   draw(context) {
-    context.fillStyle = 'black';
+    context.save();
     
+    context.translate(this.pos.x, this.pos.y);
+    context.lineWidth = 4;
+
     context.beginPath();
-    context.arc(this.pos.x, this.pos.y, this.radius, 0, Math.PI * 2);
+    context.arc(0, 0, this.radius, 0, Math.PI * 2);
     context.fill();
+    context.stroke();
+    
+    context.restore();
   }
 }
 
@@ -42,7 +65,9 @@ const sketch = ({ width, height }) => {
     context.fillRect(0, 0, width, height);
 
     agents.forEach(agent => {
+      agent.update();
       agent.draw(context);
+      agent.bounce(width, height);
     });
   };
 };
